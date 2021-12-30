7ecc4dd7368145f5193ec290f798cecd5d47ca36
Hokcu <tosimasu19@gmail.com>
2021-12-30 16:59:45 +0100

    commit7

--- src/test/java/Hsn1.java
+++ src/test/java/Hsn1.java
@@ -4,5 +4,6 @@ public class Hsn1 {
         System.out.println("Started Hasan1 branch");
         System.out.println("Second commit for Hasan1");
         System.out.println("third commit");
+        System.out.println(" ");
     }
 }
