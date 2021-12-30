917e8bb3cf72b458ae3f3d1f36c303f3fd69b6ce
Hokcu <tosimasu19@gmail.com>
2021-12-30 17:19:17 +0100

    E3

--- src/test/java/Eric1.java
+++ src/test/java/Eric1.java
@@ -2,5 +2,6 @@ public class Eric1 {
     public static void main(String[] args) {
         System.out.println("Eric's first study");
         System.out.println("Second work");
+        System.out.println("third");
     }
 }
