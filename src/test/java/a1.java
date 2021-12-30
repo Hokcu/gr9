8876fbdecdf01b47aebb7956d409f6691f656787
Hokcu <tosimasu19@gmail.com>
2021-12-30 16:52:49 +0100

    commit6

--- src/test/java/a1.java
+++ src/test/java/a1.java
@@ -2,6 +2,7 @@ public class a1 {
     public static void main(String[] args) {
         System.out.println("We started gr9 project");
         System.out.println("Second commit");
+        System.out.println("We did some changes");
 
     }
 }
